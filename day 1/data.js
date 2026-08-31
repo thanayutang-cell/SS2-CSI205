b0ef05a9524938fc513beaefdb279f539a57d3e9
thanayutang-cell <thanayut.ang@spumail.net>
2026-08-31 10:43:58 +0700

    new

---
+++ day 1/data.js
@@ -0,0 +1,35 @@
+const data = {
+    invoice: {
+        date: "02 June, 2030",
+        number: "NO. 000001"
+    },
+    billedTo: {
+        name: "Studio Shodwe",
+        address: "123 Anywhere St., AnyCity",
+        email: "hello@reallygreatsite.com"
+    },
+    from: {
+        name: "Oilvia Wilson",
+        address: "123 Anywhere St., AnyCity",
+        email: "hello@reallygreatsite.com"
+    },
+    items: [
+        {
+            name: "Logo",
+            quantity: 1,
+            price: 500
+        },
+        {
+            name: "Banner",
+            quantity: 2,
+            price: 45
+        },
+        {
+            name: "Poster",
+            quantity: 3,
+            price: 55
+        }
+    ],
+    paymentMethod: "Cash",
+    note: "Thank you for choosing us!"
+};          
